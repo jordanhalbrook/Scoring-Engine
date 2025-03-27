09448ea6ee560e70c783930470cb138ff03b20bd
Jordan Halbrook <halbrookjordan@gmail.com>
2025-03-27 15:17:28 -0400

    Added readConfig function and added checkHTTP skeleton. Added httpPage to config file.

--- scoringEngine.py
+++ scoringEngine.py
@@ -5,16 +5,36 @@ def createConfig():
     config = configparser.ConfigParser()
 
     config['General'] = {'interval': '30'}
+    config['HTTP'] = {'page': 'testpage.html'}
 
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
-def checkSMTP():
-    
+def readConfig():
+    config = configparser.ConfigParser()
+
+    config.read('config.ini')
+
+    interval = config.get('General', 'interval')
+    httpPage = config.get('HTTP', 'page')
+
+    configValues = {
+        'interval': interval,
+        'httpPage': httpPage
+    }
+
+    return configValues
+
+def checkSMTP(configData):
+    print("placeholder")
+
+def checkHTTP(configData):
+    print("placeholder")
 
 def main():
     if not os.path.isfile('config.ini'):
         createConfig()
+    configData = readConfig()
 
 if __name__=="__main__":
     main()
