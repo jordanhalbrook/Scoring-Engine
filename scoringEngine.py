9902d6df7e28e01d252c9b1ef86413624462a407
Jordan Halbrook <halbrookjordan@gmail.com>
2025-03-27 14:36:06 -0400

    Added initial code and gitignore.

---
+++ scoringEngine.py
@@ -0,0 +1,17 @@
+import configparser
+import os.path
+
+def createConfig():
+    config = configparser.ConfigParser()
+
+    config['General'] = {'interval': '30'}
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+def main():
+    if not os.path.isfile('config.ini'):
+        createConfig()
+
+if __name__=="__main__":
+    main()
